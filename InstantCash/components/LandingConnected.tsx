f1cef6927adf5d7c12990591e7f6c7c2399faaad
Julio M Cruz <julio.cruz@eb-ms.net>
2024-06-29 19:51:28 -0400

    Update LandingConnected.tsx

--- InstantCash/components/LandingConnected.tsx
+++ InstantCash/components/LandingConnected.tsx
@@ -1,6 +1,21 @@
 import { Button } from "./ui/button";
 import { useRouter } from 'next/navigation';
 
+import { createThirdwebClient, getContract, resolveMethod, ThirdwebContract } from "thirdweb";
+import { ConnectButton, ThirdwebProvider, TransactionButton } from "thirdweb/react";
+import { prepareContractCall } from "thirdweb"
+import { defineChain } from "thirdweb/chains";
+
+export const client = createThirdwebClient({ 
+  clientId: process.env.NEXT_PUBLIC_THIRDWEB_CLIENT_ID || "", 
+});
+
+export const contract = getContract({ 
+  client, 
+  chain: defineChain(84532), 
+  address: "0xc6B1cBbA50CaA1800A812156328Bb8D3BaC6f545"
+});
+
 export default function LandingConnectedComponent() {
 
   const router = useRouter();
@@ -9,6 +24,19 @@ export default function LandingConnectedComponent() {
     console.log('Deposit Collateral clicked');
     router.push('/pay');
   }
+
+  const handleSuccess = (receipt: any) => {
+    console.log('Transaction confirmed', receipt);
+    console.log('Deposit Collateral clicked');
+    router.push('/pay');
+  }  
+
+  const transaction = prepareContractCall({ 
+    contract, 
+    method: "function createPaymentLink(uint256 amount, string title, string guid)", 
+    params: [BigInt(10), "", ""]
+
+  }); 
 
   return (
     <div className="flex items-center justify-center min-h-screen bg-black">
@@ -19,7 +47,16 @@ export default function LandingConnectedComponent() {
           collateral.
         </p>
         <p className="mb-4">Credit plan: 4 monthly payments of $45 + $1.8 (interest)</p>
-        <Button className="w-full px-4 py-2 rounded-md" onClick={deposit}>Deposit Collateral</Button>
+        {/* <Button className="w-full px-4 py-2 rounded-md" onClick={deposit}>Deposit Collateral</Button> */}
+
+        <TransactionButton 
+          transaction={() => transaction}
+          onTransactionConfirmed={handleSuccess}   
+          unstyled
+        >
+          Create
+        </TransactionButton>
+
       </div>
     </div>
   )
